2b485aa66ea55c506e4fb5d5b03e02d3c3c06ca2
dwood1030 <dalton.woodard1@gmail.com>
2022-01-23 16:34:10 -0500

    basic calculator app with operation for basic calculations

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator app!");
+            Console.WriteLine("Please enter the first number of your calculation");
+
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second number of your calcucation");
+
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What kind of operation would you like to perform?");
+            Console.WriteLine("Please enter 1 for Addition");
+            Console.WriteLine("Please enter 2 for Subtraction");
+            Console.WriteLine("Please enter 3 for Multiplication");
+            Console.WriteLine("Please enter 4 for Division");
+
+            decimal operation = Convert.ToDecimal(Console.ReadLine());
+
+            if (operation == 1)
+            {
+                decimal result = numberOne + numberTwo;
+                Console.WriteLine("Your result is: " + result + "!");
+            }
+
+            else if (operation == 2)
+            {
+                decimal result = numberOne - numberTwo;
+                Console.WriteLine("Your result is: " + result + "!");
+            }
+            
+            else if (operation == 3)
+            {
+                decimal result = numberOne * numberTwo;
+                Console.WriteLine("Your result is: " + result + "!");
+            }
+
+            else if (operation == 4)
+            {
+                decimal result = numberOne / numberTwo;
+                Console.WriteLine("Your result is: " + result + "!");
+            }
+
+            else
+            {
+                Console.WriteLine("Please enter a valid operation");
+            }
+        }
+    }
+}
